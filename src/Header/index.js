dd8de0b52848fa250859481fca1547cf725fe16d
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-02-05 14:55:43 -0700

    router setup

---
+++ src/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class Header extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div id="header-banner">
+        <h1>Planet Wager</h1>
+      </div>
+    )
+  }
+}
+
+export default Header 
