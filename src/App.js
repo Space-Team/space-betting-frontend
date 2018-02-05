7e9b42851578760014328add122e981962bb635b
Emily Pancake <epancake24@gmail.com>
2018-02-05 15:20:09 -0700

    add mainpage componen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import "./App.css"
 import Header from "./Header"
+import Main from "./Main"
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Router>
+          <Route path="/main" render={()=><Main />} />
 
         </Router>
       </div>
---
+++ src/Main/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+        <div className='MainPage'>
+          <h2>Available Bets</h2>
+          <div>{}</div>
+        </div>
+    )
+  }
+
+}
+
+export default Main;
