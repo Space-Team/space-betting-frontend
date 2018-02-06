e071c063b87ee2273fad5a19dbcc0af4e2f1d62d
Emily Pancake <epancake24@gmail.com>
2018-02-06 16:00:44 -0700

    style header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
+import "./reset.css"
 import App from "./App"
 import 'antd/dist/antd.css'
+import "./index.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
