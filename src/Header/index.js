dd650e66a89e84685122d10193907ed0ccd31ecf
Emily Pancake <epancake24@gmail.com>
2018-02-06 14:22:57 -0700

    added route

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import "./style.css"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Profile from '../Profile'
 
 class Header extends React.Component {
   constructor(props){
@@ -10,6 +12,14 @@ class Header extends React.Component {
     return(
       <div id="header-banner">
         <h1>Planet Wager</h1>
+        <Router>
+          <div>
+            <Link to="/profile">
+              <img src="https://cdn4.iconfinder.com/data/icons/people-of-service/512/People_Services_astronaut_man-256.png"/>
+            </Link>
+            <Route path="/profile" render={()=><Profile />} />
+          </div>
+        </Router>
       </div>
     )
   }
