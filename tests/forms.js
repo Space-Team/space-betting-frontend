1753834df9403e49777138317655fa798c6acf85
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-08 14:59:31 -0700

    Add form tests

--- tests/forms.js
+++ tests/forms.js
@@ -14,14 +14,36 @@ describe('Form Submission', () => {
     cy.get('#form-password-confirm').type('adventurous');
     cy.get('#login-submitter').click();
     cy.get('#exists-warning').should('not.have.class', 'hidden');
-    // Login with incorrect info
-    // Should get error
-    // Login correctly
+    cy.get('#profile-dropdown').invoke('show');
+    cy.get('#profile-dropdown a').eq(0).click();
+    cy.get('#wrong-creds').should('have.class', 'hidden');
+    cy.get('#form-user-name').type('adventurous-amber');
+    cy.get('#form-password').type('incorrect password');
+    cy.get('#login-submitter').click();
+    cy.get('#wrong-creds').should('not.have.class', 'hidden');
+    cy.get('#form-user-name').clear().type('adventurous-amber');
+    cy.get('#form-password').clear().type('adventurous');
+    cy.get('#login-submitter').click();
+
     // Create a bet
-    // Bet should appear
-    // Logout/login as evil-emily
-    // Accept bet
-    // Resolve bet
+    cy.get('#post-button').click();
+    cy.get('.ant-input-number-input').clear().type('10');
+    cy.get('textarea').type('Kyle will get three diet cokes from g7-11');
+    cy.get('.postBetButton').eq(0).click();
+    cy.get('.betcard').should('contain', 'Kyle will get three diet cokes from g7-11');
+
+    cy.get('#profile-dropdown').invoke('show');
+    cy.get('#profile-dropdown a').eq(2).click();
+    cy.get('#profile-dropdown').invoke('show');
+    cy.get('#profile-dropdown a').eq(0).click();
+    cy.get('#form-user-name').type('evil-emily');
+    cy.get('#form-password').type('evil');
+    cy.get('#login-submitter').click();
+    cy.contains('div', 'Kyle will get three diet cokes from g7-11').find('button').click();
+    cy.get('body > div:nth-child(3) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > div > button.ant-btn.ant-btn-primary').click();
+    cy.get('#profile-dropdown').invoke('show');
+    cy.get('#profile-dropdown a').eq(1).click();
+    cy.contains('div', 'Kyle will get three diet cokes from g7-11').find('button').eq(0).click();
     // Logout/login (amber) / resolve bet
     // Spacebucks should update
   });
