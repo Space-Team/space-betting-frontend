0a18a1490ec9694a031dd816e90e44290a3c84e8
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-08 11:12:40 -0700

    Finish content testing

--- tests/content.js
+++ tests/content.js
@@ -1,6 +1,9 @@
 describe('Page Content', () => {
   it('Pages should display correct content', () => {
     cy.visit('/main');
+    cy.window().then((win) => {
+      win.sessionStorage.clear();
+    });
     cy.title().should('include', 'Planet Wager');
 
     // MAIN
@@ -15,14 +18,11 @@ describe('Page Content', () => {
     cy.get('small').should('contain', 'SpaceTeam Industries');
     cy.get('h2').should('contain', 'Available Bets');
     cy.get('.cardHeaders p').should('have.length', 3);
-    cy.get('.cardHeaders p').eq(0).should('have.text', 'Amount');
+    cy.get('.cardHeaders p').eq(0).should('have.text', '$$$');
     cy.get('.cardHeaders p').eq(1).should('have.text', 'Description');
     cy.get('.cardHeaders p').eq(2).should('have.text', 'Creator');
     cy.get('.maincard').eq(0).find('p').should('have.length', 3);
     cy.get('.maincard').eq(0).find('button').should('have.text', 'Accept');
-
-    // click to...
-    // Click to PROFILE section to check that it is not populated?
 
     // LOGIN
     cy.get('#profile-dropdown').invoke('show');
@@ -31,21 +31,24 @@ describe('Page Content', () => {
     cy.get('#form-user-name').type('adventurous-amber');
     cy.get('#form-password').type('adventurous');
     cy.get('#login-submitter').click();
-    // #profile-button img src changes after login
-    // navigates back to...
-    
+    // cy.get('#profile-button img').should('not.have.attr', 'src', 'http://www.janetallinger.com/images/icons/big/coin.png');
 
     // MAIN
-    // correct URL
-    // Clicking Post a Bet button removes hidden class?
-    // click to...
-    
+    cy.url().should('equal', 'http://localhost:3000/main');
+    cy.get('#root > div > div.MainPage > div.place-bets-box > section').should('have.class', 'hidden');
+    cy.get('#post-button').click();
+    cy.get('#root > div > div.MainPage > div.place-bets-box > section').should('not.have.class', 'hidden');
 
     // PROFILE
-    // correct URL
-    // username
-    // spacebucks
-    // list of bets involved in
-    // bet has creator, description, accepted, winner is creator/acceptor/wash
+    cy.get('#profile-dropdown').invoke('show');
+    cy.get('#profile-dropdown a').eq(1).click();
+    cy.url().should('equal', 'http://localhost:3000/profile');
+    cy.get('h1').eq(1).should('have.text', 'adventurous-amber');
+    cy.get('.sbucksLabel').should('have.text', 'Spacebucks:');
+    cy.get('h2').should('exist');
+    cy.get('.currbets p').should('have.length', 4);
+    cy.get('.currbets p').eq(0).should('contain', 'Created by');
+    cy.get('.currbets p').eq(1).should('contain', 'Description');
+    cy.get('.currbets p').eq(2).should('contain', 'Accepted by');
   });
 });
