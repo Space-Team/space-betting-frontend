450a252b61d0505a04dfbbaf0039d3bf2f73e2df
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-08 19:02:37 -0700

    Refactor form tests to fit layout updates

--- tests/forms.js
+++ tests/forms.js
@@ -16,10 +16,7 @@ describe('Form Submission', () => {
     cy.get('#exists-warning').should('not.have.class', 'hidden');
 
     // LOGIN
-    cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
-    cy.get('#profile-menu > a > div > i').eq(1).click();
-    cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(0).click();
-
+    cy.visit('/login');
     cy.get('#wrong-creds').should('have.class', 'hidden');
     cy.get('#form-user-name').type('adventurous-amber');
     cy.get('#form-password').type('incorrect password');
@@ -37,33 +34,55 @@ describe('Form Submission', () => {
     cy.get('.betcard').should('contain', 'Kyle will get three diet cokes from g7-11');
 
     // LOGOUT
-    // cy.get('#profile-dropdown').invoke('show');
-    // cy.get('#profile-dropdown a').eq(2).click();
-    cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
-    cy.get('#profile-menu > a > div > i').eq(1).click();
-    cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(1).click();
-
-    // LOGIN
-    // cy.get('#profile-dropdown').invoke('show');
-    // cy.get('#profile-dropdown a').eq(0).click();
     cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
     cy.get('#profile-menu > a > div > i').eq(1).click();
     cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(0).click();
 
+    // LOGIN
+    // cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
+    // cy.get('#profile-menu > a > div > i').eq(1).click();
+    // cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(0).click();
+    cy.visit('/login');
+
     cy.get('#form-user-name').type('evil-emily');
     cy.get('#form-password').type('evil');
     cy.get('#login-submitter').click();
+
+    // ACCEPT BET
     cy.contains('div', 'Kyle will get three diet cokes from g7-11').find('button').click();
     cy.get('body > div:nth-child(3) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > div > button.ant-btn.ant-btn-primary').click();
+
     // PROFILE
-    // cy.get('#profile-dropdown').invoke('show');
-    // cy.get('#profile-dropdown a').eq(1).click();
-    cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
-    cy.get('#profile-menu > a > div > i').eq(1).click();
-    cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(2) > a').eq(0).click();
+    // cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
+    // cy.get('#profile-menu > a > div > i').eq(1).click();
+    // cy.get('body > div:nth-child(2) > div > div > ul > li:nth-child(2) > a').eq(0).click();
+    cy.visit('/profile');
 
     cy.contains('div', 'Kyle will get three diet cokes from g7-11').find('button').eq(0).click();
-    // Logout/login (amber) / resolve bet
+    // ISSUE - accepting vs resolving
+    cy.get('body > div:nth-child(3) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > div > button.ant-btn.ant-btn-primary').click();
+
+    // LOGOUT
+    cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
+    cy.get('#profile-menu > a > div > i').eq(1).click();
+    cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(0).click();
+
+    // LOGIN
+    // cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
+    // cy.get('#profile-menu > a > div > i').eq(1).click();
+    // cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(1) > a').eq(0).click();
+    cy.visit('/login');
+    cy.get('#form-user-name').type('adventurous-amber');
+    cy.get('#form-password').type('adventurous');
+    cy.get('#login-submitter').click();
+
+    // Resolve bet
+    // cy.get('a.ant-dropdown-link.hide.ant-dropdown-trigger').invoke('show');
+    // cy.get('#profile-menu > a > div > i').eq(1).click();
+    // cy.get('body > div:nth-child(3) > div > div > ul > li:nth-child(2) > a').eq(0).click();
+    cy.visit('/profile');
+    cy.contains('div', 'Kyle will get three diet cokes from g7-11').find('button').eq(1).click();
+
     // Spacebucks should update
   });
 });
