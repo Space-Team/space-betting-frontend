ec2b6439072dd4b41022bb3061fdbb0512b9fbb4
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-09 12:03:19 -0700

    Finish tests

--- tests/forms.js
+++ tests/forms.js
@@ -14,8 +14,6 @@ describe('Form Submission', () => {
     cy.get('#form-password-confirm').type('adventurous');
     cy.get('#login-submitter').click();
     cy.get('#exists-warning').should('not.have.class', 'hidden');
-
-    // LOGIN
     cy.visit('/login');
     cy.get('#wrong-creds').should('have.class', 'hidden');
     cy.get('#form-user-name').type('adventurous-amber');
@@ -25,20 +23,14 @@ describe('Form Submission', () => {
     cy.get('#form-user-name').clear().type('adventurous-amber');
     cy.get('#form-password').clear().type('adventurous');
     cy.get('#login-submitter').click();
-
-    // Create a bet
-    // cy.get('#post-button').click();
-    // cy.get('.ant-input-number-input').clear().type('10');
-    // cy.get('textarea').type('Kyle will get three diet cokes from g7-11');
-    // cy.get('.postBetButton').eq(0).click();
-    // cy.get('.betcard').should('contain', 'Kyle will get three diet cokes from g7-11');
-
-    // ACCEPT BET???
-
-    // Resolve a bet
+    cy.get('#post-button').click();
+    cy.get('.ant-input-number-input').clear().type('10');
+    cy.get('textarea').type('Kyle will get three diet cokes from g7-11');
+    cy.get('.postBetButton').eq(0).click();
+    cy.get('.betcard').should('contain', 'Kyle will get three diet cokes from g7-11');
     cy.get('#profile-menu > a > i').click();
     cy.contains('Profile').click();
-
-    // Spacebucks should update
+    cy.contains('.currbets', "Everyone in our group will throw up").find('button').eq(0).click();
+    cy.contains('Collect 16 Spacebucks').click();
   });
 });
