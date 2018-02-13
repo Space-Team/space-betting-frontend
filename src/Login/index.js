4ea348ebeb42e7a6edfd6b9923d3096730ddff4e
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-02-12 22:15:45 -0700

    changing to state

--- src/Login/index.js
+++ src/Login/index.js
@@ -2,6 +2,15 @@ import React from "react"
 import "./style.css"
 
 class Login extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      login: false
+    }
+    this.login = this.login.bind(this)
+    this.changeUsername = this.changeUsername.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+  }
 
   forgot(){
     console.log("tough tootie!")
@@ -12,18 +21,32 @@ class Login extends React.Component {
     console.log("create user redirect: ", window.location.href = "/new-user")
   }
 
+  changeUsername(e) {
+    this.setState({username: e.target.value})
+  }
+
+
+  changePassword(e) {
+    this.setState({password: e.target.value})
+  }
+
+  login(e) {
+    alert('A form was submitted: ' + this.state.username)
+    alert('A form was submitted: ' + this.state.password)
+    e.preventDefault()
+  }
 
   render(){
     return(
       <div id="login-container">
         <h2 className='welcome'>Welcome... Login for Liftoff</h2>
-        <form id="login-box" onSubmit={this.props.validate}>
+        <form id="login-box" onSubmit={this.login}>
 
           <label htmlFor="login-user-name">Username: </label>
-          <input type="text" id="form-user-name" name="userName" />
+          <input type="text" id="form-user-name" value={this.state.value} onChange={this.changeUsername} />
 
           <label htmlFor="login-password">Password: </label>
-          <input type="password" id="form-password" name="userPass" />
+          <input type="password" id="form-password" value={this.state.password} onChange={this.changePassword} />
 
           <button className="login-form-btn" id="forgot" type="button" onClick={this.forgot}>Forgot Something?</button>
 
