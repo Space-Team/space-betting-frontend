edc72ba32ff6853bdf7ced938fae3c79130a586d
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-02-13 00:10:53 -0700

    ITS ALIVE

--- src/CreateUser/index.js
+++ src/CreateUser/index.js
@@ -9,7 +9,14 @@ class CreateUser extends React.Component {
     this.state = {
       unique: true,
       match: false,
-      users: []
+      users: [],
+      firstname: "",
+      lastname: "",
+      name: "",
+      image: 0,
+      spacebucks: 100,
+      password: "",
+      passTwo: ""
     }
     this.register = this.register.bind(this)
     this.checkName = this.checkName.bind(this)
@@ -38,20 +45,43 @@ class CreateUser extends React.Component {
     }
   }
 
+  changeFn = (e) => {
+    this.setState({ firstname: e.target.value })
+  }
+
+  changeLn = (e) => {
+    this.setState({ lastname: e.target.value })
+  }
+
+  changeUn = (e) => {
+    this.setState({ name: e.target.value })
+  }
+
+  changeAv = (e) => {
+    this.setState({ image: e.target.value })
+  }
+
+  changePw = (e) => {
+    this.setState({ password: e.target.value})
+  }
+
+  changeP2 = (e) => {
+    this.setState({ passTwo: e.target.value })
+  }
+
   register(e){
     e.preventDefault()
     window.sessionStorage.clear()
-    var form = new FormData(e.target)
+    this.checkName(this.state.name)
+    this.checkPass(this.state.password, this.state.passTwo)
     var sender = {
-      firstName: form.get("firstName"),
-      lastName: form.get("lastName"),
-      name: form.get("userName"),
-      image: form.get("avatar"),
+      firstName: this.state.firstname,
+      lastName: this.state.lastname,
+      name: this.state.name,
+      image: this.state.image,
       spacebucks: 100,
-      password: form.get("password")
+      password: this.state.password
     }
-    this.checkName(sender.name)
-    this.checkPass(sender.password, form.get("passwordConfirm"))
     setTimeout(()=>{
     if (this.state.unique && this.state.match){
       fetch(apiUrl, {
@@ -75,43 +105,44 @@ class CreateUser extends React.Component {
         <form id="register-box" onSubmit={this.register}>
           <h3>Register A New User</h3>
           <label htmlFor="register-first-name">First Name: </label>
-          <input type="text" id="register-first-name" name="firstName" />
+          <input type="text" id="register-first-name" onChange={this.changeFn} value={this.state.firstname} />
 
           <label htmlFor="register-last-name">Last Name: </label>
-          <input type="text" id="register-last-name" name="lastName" />
+          <input type="text" id="register-last-name" onChange={this.changeLn} value={this.state.lastname}/>
 
-          <label htmlFor="register-user-name">Choose a Username: </label>
-          <input type="text" id="register-user-name" name="userName" />
+          <label htmlFor="register-newt-name">User Name: </label>
+          <input type="text" id="register-newt-name" onChange={this.changeUn} value={this.state.name}/>
+
           <p id="exists-warning" className="hidden">Username already exists...</p>
 
           <label htmlFor="avatarChoice">Select an avatar: </label>
           <div id="avatarChoice">
             <label>
-              <input type="radio" id="avatarChoice1" name="avatar" value="1"/>
+              <input type="radio" id="avatarChoice1" name="avatar"  onChange={this.changeAv} value="1"/>
               <img className="avatar-needs" src="./astronaut.png" alt="astronaut avatar" />
             </label>
 
             <label>
-              <input type="radio" id="avatarChoice2" name="avatar" value="2"/>
+              <input type="radio" id="avatarChoice2" name="avatar" onChange={this.changeAv} value="2"/>
               <img className="avatar-needs" src="./dog.png" alt="dog astronaut avatar"/>
             </label>
 
             <label>
-              <input type="radio" id="avatarChoice3" name="avatar" value="3"/>
+              <input type="radio" id="avatarChoice3" name="avatar" onChange={this.changeAv} value="3"/>
               <img className="avatar-needs" src="./alien.png" alt="alien avatar"/>
             </label>
 
             <label>
-              <input type="radio" id="avatarChoice4" name="avatar" value="4"/>
+              <input type="radio" id="avatarChoice4" name="avatar" onChange={this.changeAv} value="4"/>
               <img className="avatar-needs" src="./robot.png" alt="robot avatar"/>
             </label>
           </div>
 
           <label htmlFor="form-password">Password: </label>
-          <input type="password" id="form-password" name="password" />
+          <input type="password" id="form-password" onChange={this.changePw}/>
 
           <label htmlFor="form-password-confirm">Confirm Password: </label>
-          <input type="password" id="form-password-confirm" name="passwordConfirm" />
+          <input type="password" id="form-password-confirm" onChange={this.changeP2} value={this.state.passTwo} />
           <p id="mismatch-warning" className="hidden">Your passwords do not match...</p>
 
           <button id="login-submitter" type="submit" >Register!</button>
